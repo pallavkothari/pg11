32192059d59d99600f55a4ac3da90b7729aed2f1
pallav.kothari <pallavkothari@gmail.com>
2019-05-01 17:35:37 -0700

    install & dist

--- pg11.sh
+++ pg11.sh
@@ -5,6 +5,18 @@ set -x
 
 PG_ROOT=/tmp/pg11
 PG_HOME=$PG_ROOT/postgresql-11.2
+INSTALL_DIR=$PG_ROOT/install
+DIST_DIR=$PG_ROOT/dist
+PACKAGE_NAME=postgresql-Linux-x86_64.txz
+
+darwin=false
+case "`uname`" in
+  Darwin* )
+    darwin=true
+    PACKAGE_NAME=postgresql-Darwin-x86_64.txz
+    ;;
+esac
+
 
 downloadSourceCode() {
 	[ -e $PG_ROOT ] && echo "$PG_ROOT already exists - skipping download" && return 0;
@@ -22,16 +34,32 @@ build() {
 	pushd $PG_ROOT
 	mkdir -p build_dir
 	cd build_dir
-	$PG_HOME/configure
+	$PG_HOME/configure --prefix $INSTALL_DIR
 	echo "config log is available at $PG_ROOT/build_dir/config.log"
 	echo "starting build"
 	make
+	## this installs to the $INSTALL_DIR specified above
+	make install
+	popd
+}
+
+package() {
+	pushd $INSTALL_DIR
+	tar cJF $DIST_DIR/$PACKAGE_NAME \
+	  share/postgresql \
+	  lib \
+	  bin/initdb \
+	  bin/pg_ctl \
+	  bin/postgres
+	echo "created postgres 11 binary at $DIST_DIR"
 	popd
 }
 
 main() {
 	downloadSourceCode
 	build
+	package
 }
 
 main
+
